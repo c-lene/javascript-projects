40d6d46a114dff46628169e2b253b4de1c38447f
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-06-19 15:34:49 -0500

    completed & added comments to Arrays - part 1 Exercises

--- arrays/exercises/part-one-arrays-CT.js
+++ arrays/exercises/part-one-arrays-CT.js
@@ -3,13 +3,13 @@ let practiceFile = [273.15];
 
 
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
-practiceFile.push(42);
+practiceFile.push(42);                                          // ADDS 42 to the END of the Array
 console.log(practiceFile);
 
-practiceFile.push("hello");
+practiceFile.push("hello");                                     // ADDS "hellow" to the END of the Array
 console.log(practiceFile);
 
 
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
-practiceFile.push(false, -4.6, "87");
+practiceFile.push(false, -4.6, "87");                           // ADDS false, -4.6, "87" to the END of the Array
 console.log(practiceFile);
