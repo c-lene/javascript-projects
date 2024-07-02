a6bbbcdb754a72189a25d88616b5a4585c871506
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-02 02:01:52 -0500

    completed part 2 - working on Bonus

--- more-on-functions/studio/part-two-create-sorted-array-CT.js
+++ more-on-functions/studio/part-two-create-sorted-array-CT.js
@@ -26,24 +26,44 @@ function sortMinValue(value) {
        
         while (value.length > 0) {
         sortedArray.push(findMinValue(value));
-        value.splice(value.indexOf(findMinValue(value), 1));
+        value.splice(value.indexOf(findMinValue(value)), 1);
     }
     
-    sortedArray.push(findMinValue(value));
+    //sortedArray.push(findMinValue(value));
+    console.log(value)
     return sortedArray
 }
 
 
 
-/* BONUS MISSION: Refactor your sorting function to use recursion below:
- */
+/* BONUS MISSION: Refactor your sorting function to use recursion below: */
+
+/*
+function sortMinValueRecursion(value) {
+  if (value.length < 0) {
+    //solve last small step
+
+    //end Recursion
+  } else {
+    //call function again
+  }
+}
+*/
+
+
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-//let newSortedArray = sortedArray.map(sortMinValue(nums1))
 
-console.log(nums1);
-console.log(sortMinValue(nums1));
+
+//console.log(nums1);
+//console.log(sortMinValue(nums1));
+
+//console.log(nums2);
+//console.log(sortMinValue(nums2));
+
+console.log(nums3);
+console.log(sortMinValue(nums3));
