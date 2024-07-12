1055d50336b0587739e56e5ef68ddb9082d622d8
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-12 00:26:08 -0500

    completed  exporting isPalindrome function example

---
+++ modules/exporting-modules/index-CT.js
@@ -0,0 +1,7 @@
+// Import the modules exported from practiceExports.js below:
+
+const palindromeCheck = require('./practiceExports-CT.js');
+
+console.log(typeof palindromeCheck);
+console.log(palindromeCheck('that'));
+console.log(palindromeCheck('radar'));
