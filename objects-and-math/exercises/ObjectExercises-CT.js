27d2507c7039cacd07e76fe8484b94035ce9618a
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-12 23:04:50 -0500

    completed assigning 'astronautID'

--- objects-and-math/exercises/ObjectExercises-CT.js
+++ objects-and-math/exercises/ObjectExercises-CT.js
@@ -39,6 +39,42 @@ let tardigrade = {
 
 // After you have created the other object literals, add the astronautID property to each one.
 
+let randomNumArray = [];
+let minNum = 1
+let maxNum = 10
+ 
+
+while (randomNumArray.length < maxNum) {
+   let randomNum;
+   randomNum = Math.floor(Math.random() * maxNum) + minNum;
+   
+   // Checks if the randomized number 'randomNum' is already in the 'randomNumArray'
+   let checkDuplicates = randomNumArray.includes(randomNum);
+
+   // Using If statement to check for duplicates - No Duplicates = "FALSE" - then will add 'randomNum' to the 'randomNumArray'
+   if (checkDuplicates === false){                                            
+      randomNumArray.push(randomNum);
+   } 
+}
+
+console.log(randomNumArray);
+
+// Assigning Randomized Numbers as "astronautID" for each species - NO Crew members should have the same ID.
+superChimpOne["astronautID"] = randomNumArray[0];
+console.log(superChimpOne);
+
+salamander["astronautID"] = randomNumArray[1];
+console.log(salamander);
+
+superChimpTwo["astronautID"] = randomNumArray[2];
+console.log(superChimpTwo);
+
+beagle["astronautID"] = randomNumArray[3];
+console.log(beagle);
+
+tardigrade["astronautID"] = randomNumArray[4];
+console.log(tardigrade);
+
 
 
 // Add a move method to each animal object
