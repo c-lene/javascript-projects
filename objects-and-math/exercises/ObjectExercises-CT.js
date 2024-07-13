4a7ef27eb3e7a87fca2cb89ab59e7ed36bf3ee6e
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-13 01:32:15 -0500

    completed Part 2: Crew Reports

--- objects-and-math/exercises/ObjectExercises-CT.js
+++ objects-and-math/exercises/ObjectExercises-CT.js
@@ -2,14 +2,14 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
 };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
 };
 
 
@@ -17,7 +17,7 @@ let superChimpTwo = {
    name: "Brad",
    species: "Chimpanzee",
    mass: 11,
-   age: 6
+   age: 6,
 };
 
 
@@ -25,14 +25,15 @@ let beagle = {
    name: "Leroy",
    species: "Beagle",
    mass: 14,
-   age: 5
+   age: 5,
 };
 
 
 let tardigrade = {
    name: "Almina",
    species: "Tardigrade",
-   mass: 0.0000000001
+   mass: 0.0000000001,
+   age: 1,
 };
 
 
@@ -60,27 +61,74 @@ while (randomNumArray.length < maxNum) {
 console.log(randomNumArray);
 
 // Assigning Randomized Numbers as "astronautID" for each species - NO Crew members should have the same ID.
-superChimpOne["astronautID"] = randomNumArray[0];
+superChimpOne.astronautID = randomNumArray[0];
+// For Checking purposes 
+//console.log(superChimpOne);                                        
+
+salamander.astronautID = randomNumArray[1];
+// For Checking purposes 
+//console.log(salamander);
+
+superChimpTwo.astronautID = randomNumArray[2];
+// For Checking purposes 
+//console.log(superChimpTwo);
+
+beagle.astronautID = randomNumArray[3];
+// For Checking purposes 
+//console.log(beagle);
+
+tardigrade.astronautID = randomNumArray[4];
+// For Checking purposes 
+//console.log(tardigrade);
+
+
+
+// Add a move method to each animal object --------------------------------------
+//Functions that assigns a Random Number of steps from 0 - 10
+function moveSteps () {
+   let steps = Math.floor(Math.random()*11);
+   return steps;
+};
+
+// Creating & Adding the 'move' method to each animal object 
+superChimpOne.move = moveSteps;
 console.log(superChimpOne);
+console.log(`${superChimpOne.name} will take ${superChimpOne.move()} steps.`);
 
-salamander["astronautID"] = randomNumArray[1];
+
+salamander.move = moveSteps;
 console.log(salamander);
+console.log(`${salamander.name} will take ${salamander.move()} steps.`);
 
-superChimpTwo["astronautID"] = randomNumArray[2];
+
+superChimpTwo.move = moveSteps;
 console.log(superChimpTwo);
+console.log(`${superChimpTwo.name} will take ${superChimpTwo.move()} steps.`);
 
-beagle["astronautID"] = randomNumArray[3];
+
+beagle.move = moveSteps;
 console.log(beagle);
+console.log(`${beagle.name} will take ${beagle.move()} steps.`);
 
-tardigrade["astronautID"] = randomNumArray[4];
+
+tardigrade.move = moveSteps;
 console.log(tardigrade);
+console.log(`${tardigrade.name} will take ${tardigrade.move()} steps.`);
 
 
-
-// Add a move method to each animal object
 
 // Create an array to hold the animal objects.
+let crew = [superChimpOne, salamander, superChimpTwo, beagle, tardigrade];
+console.log(crew);
+
 
 // Print out the relevant information about each animal.
+function crewReports(animal) {
+   console.log(`${animal.name} is a ${animal.species}. They are ${animal.age} years old and ${animal.mass} kilograms. Their ID is ${animal.astronautID}.`);
+}
+
+
+let mappedArrayCrew = crew.map(crewReports);
+console.log(mappedArrayCrew);
 
 // Start an animal race!
