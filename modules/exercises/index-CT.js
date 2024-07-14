dc5ac80e5f7791b6b9e2ac8fa41bfb53a1bd33b5
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-13 19:54:19 -0500

    completed portions of 'Finish the Project' & completed successful test output

--- modules/exercises/index-CT.js
+++ modules/exercises/index-CT.js
@@ -15,7 +15,7 @@ let scores = [[95, 86, 83, 81, 76],[79, 71, 79, 87, 72],[94, 87, 87, 83, 82],[99
 let prompts = ['display all scores', 'average the scores for each test', 'average the scores for each astronaut','select the next spacewalker'];
 
 for (let i = 0; i<prompts.length; i++){
-  let response = input.question(`Would you like to ${prompts[i]}? Y/N: `);
+  let response = input.question(`\nWould you like to ${prompts[i]}? Y/N: `);
   if (response.toLowerCase()==='y'){
     if (i===0){
       
@@ -24,17 +24,17 @@ for (let i = 0; i<prompts.length; i++){
 
     } else if (i===1){
       for (let j = 0; j<testTitles.length; j++){
-        let avg = //Call 'averageForTest' here. Pass in j and scores as arguments.
+        let avg = averages.averageForTest(j, scores)                    //Call 'averageForTest' here. Pass in j and scores as arguments.
         console.log(`${testTitles[j]} test average = ${avg}%.`);
       }
     } else if (i===2){
       for (let j = 0; j<astronauts.length; j++){
-        let avg = //Call 'averageForStudent' here. Pass in j and scores as arguments.
+        let avg = averages.averageForStudent(j, scores);                //Call 'averageForStudent' here. Pass in j and scores as arguments.
         console.log(`${astronauts[j]}'s test average = ${avg}%.`);
       }
     } else {
-      let walker = //Call 'randomSelect' to pick a spacewalker from the astronauts array.
-      console.log(`${walker} is the next spacewalker.`);
+      let walker = randomSelect(astronauts);                            //Call 'randomSelect' to pick a spacewalker from the astronauts array.
+      console.log(`${walker} is the next spacewalker.\n`);
     }
   } else {
     console.log("Option skipped.");
