768d61a1964cc127e133ca650b1cd2579197bf78
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-16 00:11:47 -0500

    created duplicate for CT edit

---
+++ unit-testing/exercises-CT/tests/checkFive.test.js
@@ -0,0 +1,19 @@
+const checkFive = require('../checkFive.js');
+
+describe("checkFive", function(){
+
+    test("checks if produces correct output when passed a number LESS than 5", function() {
+        let output = checkFive(2);
+        expect(output).toEqual("2 is less than 5.");
+    });
+
+    test("checks if produces correct output when passed a number GREATER than 5.", function(){
+        output = checkFive(6);
+        expect(output).toEqual("6 is greater than 5.");
+    });
+
+    test("checks if produces correct output when passed a number EQUAL to 5.", function(){
+        output = checkFive(5);
+        expect(output).toEqual(`5 is equal to 5.`);
+    });
+});
