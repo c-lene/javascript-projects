124a7e62f605ce87c28c3af339e206f282b57b4e
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-07-18 23:46:16 -0500

    still working

--- exceptions/exercises-CT/test-student-labs.js
+++ exceptions/exercises-CT/test-student-labs.js
@@ -2,6 +2,15 @@ function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
     let result = lab.runLab(3);
+    
+    // Added a Try/Catch block to catch an Exception if the 'runLab' property is not defined.
+    try {
+      //
+    } catch(err) {
+      //
+    }
+
+
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
@@ -21,4 +30,33 @@ let studentLabs = [
   }
 ];
 
+
+// Created new array called 'studenLabs2'
+let studentLabs2 = [
+  {
+    student: 'Blake',
+    myCode: function(num) {
+      return Math.pow(num, num);
+    }
+  },
+  {
+    student: 'Jessica',
+    runLab: function(num) {
+      return Math.pow(num, num);
+    }
+  },
+  {
+    student: 'Mya',
+    runLab: function(num) {
+      return num * num;
+    }
+  },
+];
+
+
+
+
 gradeLabs(studentLabs);
+
+// Testing & Run 'gradeLabs' function using objects in 'studentLabs2'
+gradeLabs(studentLabs2);
